23c20bf2a0bf4a848cdfc399d02fb7c617792392
Julissa Bonilla <julissab9@gmail.com>
2023-02-22 23:47:37 -0600

    working through server using async

--- server.js
+++ server.js
@@ -14,30 +14,104 @@ const db = mysql.createConnection(
 
 const viewAllDepartments = async () => {
     try{
-    const [results] = await connection.promise().query('Select * FROM department')
-    console.table(results)
-    menuPrompt()
-} catch(err) {
-    throw new Error(err)
-}
-}
+        const [results] = await connection.promise().query('SELECT * FROM department')
+        console.table(results)
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
 
-const viewAllRoles
+const viewAllRoles = async () => {
+    try{
+        const [results] = await connection.promise().query('SELECT * FROM role')
+        console.table(results)
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
 
-const viewAllEmployees
+const viewAllEmployees = async () => {
+    try{
+        const [results] = await connection.promise().query('SELECT * FROM employee')
+        console.table(results)
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
 
-const addDepartment
+const addDepartment = async () => {
+    const response = await inquirer.prompt([
+        {
+            type: "input",
+            name: "deptName",
+            message: "What is the name of the department: "
+        }
+    ])
 
-const addRole
+    try{
+        const [results] = await connection.promise().query('INSERT INTO department (deptName) VALUES (?)', response.deptName)
+        console.table("Department added successfully!")
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
 
-const addEmployee
+const addRole = async () => {
+    const response = await inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Please enter the name of the Role: "
+        }, {
+            type: "input",
+            name: "salary",
+            message: "Please enter the Salary for the role: "
+        }, {
+            type: "input",
+            name: "department_id",
+            message: "Please provide the department Id for the role:"
+        }
+    ])
 
-const updateEmployee
+    try{
+        const [results] = await connection.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [response.title, response.salary, response.department_id])
+        console.table("Role information added successfully!")
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
 
-//view all depts
-//view all roles 
-//view all employees
-//add dept 
-//add role 
-//add employee 
-//update employee
+const addEmployee = async () => {
+    const response = await inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "Please enter employees's first Name: "
+        },  {
+            type: "input",
+            name: "lastName",
+            message: "Please enter employees Last name: "
+        }, {
+            type: "input",
+            name: "role_id",
+            message: "Please provide the role Id for this employee:"
+        },  {
+            type: "input",
+            name: "manager",
+            message: "Who is the employees Manager: "
+        }
+    ])
+
+    try{
+        const [results] = await connection.promise().query('INSERT INTO employee (firstName, lastName, role_id, manager) VALUES (?, ?, ?, ?)', response.firstName, response.lastName, response.role_id, response.manager)
+        console.table("Employee information added successfully!")
+        menuPrompt()
+    } catch(err) {
+        throw new Error(err)
+    }
+};
